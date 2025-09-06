18541a157d076e0c75d7bd4bb9f9cfb75f2dea5c
Eric Francis Daria <eric@extremekartz.com>
2025-09-07 07:46:31 +0800

    Added longest_common_prefix challenge

---
+++ solutions/longest_common_prefix.py
@@ -0,0 +1,31 @@
+def find_common_prefix() -> str:
+    words = ["aa", "aab", "acc"]
+
+    # If the list is empty, return an empty string
+    if not words:
+        return ""
+
+    # Sort the list of strings lexicographically (alphabetical order)
+    # The common prefix will be shared between the first and last strings
+    words.sort()
+    print(words)  # Debug: see the sorted list
+
+    first_str = words[0]  # First string after sorting
+    last_str = words[-1]  # Last string after sorting
+    print(first_str, last_str)  # Debug: see which two strings are compared
+    print(len(first_str), len(last_str))  # Debug: length
+
+    common_prefix = []
+
+    # Compare characters of first and last string until mismatch
+    for i in range(min(len(first_str), len(last_str))):
+        if first_str[i] != last_str[i]:
+            print(f"Mismatch: {i}. First: {first_str[i]}. Last: {last_str[i]}")
+            break
+        common_prefix.append(first_str[i])
+
+    # Join collected characters into the prefix string
+    common_prefix = "".join(common_prefix)
+    print(common_prefix)  # Debug: see the final common prefix
+
+    return common_prefix
